de0b1c4bc116e478abeb87d0728a32b67bed7abc
Kayte <kaytemcdonough@gmail.com>
2021-06-21 18:54:34 -0400

    added styling icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Components/Components/Navbar";
-import { Container } from 'reactstrap';
-import About from './Components/Pages/AboutMe';
-import Projects from './Components/Pages/Projects';
-import Contact from './Components/Pages/Contact';
+import { Container } from "reactstrap";
+import About from "./Components/Pages/AboutMe";
+import Projects from "./Components/Pages/Projects";
+import Contact from "./Components/Pages/Contact";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope, faCodeBranch } from "@fortawesome/free-solid-svg-icons";
 
-
-
+library.add(faEnvelope, faCodeBranch);
 
 function App() {
   return (
-      <div className="App">
-        <Navbar />
-          <Container>
-            <Route exact path="/" component={About} />
-            <Route path="/projects" component={Projects} />
-            <Route path="/contact" component={Contact} />
-          </Container>
-      </div>
+    <div className="App">
+      <Navbar />
+      <Container>
+        <Route exact path="/" component={About} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+      </Container>
+    </div>
   );
 }
 
