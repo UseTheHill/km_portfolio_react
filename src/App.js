46f35706f5fa9a695ed71a4206a5fc6349346ea5
Kayte <kaytemcdonough@gmail.com>
2021-06-22 18:34:53 -0400

    updated manifest to have the proper homepage

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Components/Components/Navbar";
-import { Container } from "reactstrap";
+// import { Container } from "reactstrap";
 import About from "./Components/Pages/AboutMe";
 import Projects from "./Components/Pages/Projects";
 import Contact from "./Components/Pages/Contact";
@@ -15,6 +15,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        
         <Navbar />
 
         <Route exact path="/" component={About} />
